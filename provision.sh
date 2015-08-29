b1ef489b904123b35030a5e5784090f4f80a6cdf
Lawrence Wakefield <lawrence@iamlawrence.me>
2015-08-29 17:13:55 +1000

    Making it my own!

--- provision.sh
+++ provision.sh
@@ -1,103 +1,50 @@
 #!/usr/bin/env bash
 export DEBIAN_FRONTEND=noninteractive
 
-sudo aptitude update -q
+sudo aptitude update
 
-# Force a blank root password for mysql
-echo "mysql-server mysql-server/root_password password " | debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password " | debconf-set-selections
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password secret'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password secret'
 
-# Install mysql, nginx, php5-fpm
-sudo aptitude install -q -y -f mysql-server mysql-client nginx php5-fpm
+sudo aptitude install -y -f mysql-server mysql-client nginx php5-fpm
 
-# Install commonly used php packages
-sudo aptitude install -q -y -f php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xcache
+sudo aptitude install -y -f php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xcache
 
-sudo rm /etc/nginx/sites-available/default
-sudo touch /etc/nginx/sites-available/default
-
-sudo cat >> /etc/nginx/sites-available/default <<'EOF'
+sudo mkdir -p /var/www/adminer/
+sudo wget http://www.adminer.org/latest-en.php -O /var/www/adminer/index.php
+sudo touch /etc/nginx/sites-available/adminer
+sudo cat >> /etc/nginx/sites-available/adminer <<'EOF'
 server {
-  listen   80;
 
-  root /usr/share/nginx/html;
-  index index.php index.html index.htm;
-
-  # Make site accessible from http://localhost/
-  server_name _;
-
-  location / {
-    # First attempt to serve request as file, then
-    # as directory, then fall back to index.html
-    try_files $uri $uri/ /index.html;
-  }
-
-  location /doc/ {
-    alias /usr/share/doc/;
-    autoindex on;
-    allow 127.0.0.1;
-    deny all;
-  }
-
-  # redirect server error pages to the static page /50x.html
-  #
-  error_page 500 502 503 504 /50x.html;
-  location = /50x.html {
-    root /usr/share/nginx/html;
-  }
-
-  # pass the PHP scripts to FastCGI server listening on /tmp/php5-fpm.sock
-  #
-  location ~ \.php$ {
-    try_files $uri =404;
-    fastcgi_split_path_info ^(.+\.php)(/.+)$;
-    fastcgi_pass unix:/var/run/php5-fpm.sock;
-    fastcgi_index index.php;
-    include fastcgi_params;
-  }
-
-  # deny access to .htaccess files, if Apache's document root
-  # concurs with nginx's one
-  #
-  location ~ /\.ht {
-    deny all;
-  }
-
-  ### phpMyAdmin ###
-  location /phpmyadmin {
-    root /usr/share/;
+    listen   80;
+    root /var/www/adminer;
     index index.php index.html index.htm;
-    location ~ ^/phpmyadmin/(.+\.php)$ {
-      client_max_body_size 4M;
-      client_body_buffer_size 128k;
-      try_files $uri =404;
-      root /usr/share/;
+    server_name adminer.dev;
 
-      # Point it to the fpm socket;
-      fastcgi_pass unix:/var/run/php5-fpm.sock;
-      fastcgi_index index.php;
-      fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-      include /etc/nginx/fastcgi_params;
+    location / {
+        try_files $uri $uri/ /index.html;
     }
 
-    location ~* ^/phpmyadmin/(.+\.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt)) {
-      root /usr/share/;
+    location ~ \.php$ {
+        try_files $uri =404;
+        fastcgi_pass unix:/var/run/php5-fpm.sock;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        include fastcgi_params;
     }
-  }
-  location /phpMyAdmin {
-    rewrite ^/* /phpmyadmin last;
-  }
-  ### phpMyAdmin ###
+
 }
 EOF
 
-sudo touch /usr/share/nginx/html/info.php
-sudo cat >> /usr/share/nginx/html/info.php <<'EOF'
-<?php phpinfo(); ?>
+sudo rm /etc/nginx/sites-enabled/default
+sudo touch /etc/nginx/sites-enabled/default
+sudo cat >> /etc/nginx/sites-enabled/default <<'EOF'
+include /etc/nginx/sites-available/adminer;
 EOF
-
-sudo aptitude install -q -y -f phpmyadmin
 
 sudo service nginx restart
 
 sudo service php5-fpm restart
+
+sudo aptitude install -y -f git zsh
+#sudo zsh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
